01c9c27b3c6f359fa191cb956ff0af14638bed61
cm3labs@gmail.com <cm3labs@gmail.com>
2024-06-02 21:40:13 +0000

    homework step - stg_staging__fhv_data.sql freshly created (no edits), made from schema.yml link

---
+++ taxi_rides_ny/models/staging/staging/stg_staging__fhv_data.sql
@@ -0,0 +1,25 @@
+
+with 
+
+source as (
+
+    select * from {{ source('staging', 'fhv_data') }}
+
+),
+
+renamed as (
+
+    select
+        dispatching_base_num,
+        pickup_datetime,
+        dropoff_datetime,
+        pulocationid,
+        dolocationid,
+        sr_flag,
+        affiliated_base_number
+
+    from source
+
+)
+
+select * from renamed
